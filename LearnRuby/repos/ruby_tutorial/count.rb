e38fdcbcfbade3a138d5dabf89f8dc7387616087
Paul Tobin <tobster111@gmail.com>
2022-07-06 19:11:29 +0100

    uniques hash words

--- LearnRuby/repos/ruby_tutorial/count.rb
+++ LearnRuby/repos/ruby_tutorial/count.rb
@@ -12,3 +12,6 @@ Love alters not with his brief hours and weeks,
 But bears it out even to the edge of doom.
 If this be error and upon me proved,
 I never writ, nor no man ever loved."
+
+uniques = {}
+words = sonnet.scan(/\w+/)
