c6fa3e12241e980126c05408ed65881586bb7c80
Paul Tobin <tobster111@gmail.com>
2022-07-06 18:53:09 +0100

    update

--- LearnRuby/repos/ruby_tutorial/hello_app.rb
+++ LearnRuby/repos/ruby_tutorial/hello_app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
-  'hello, world!'
+  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+              "Thursday", "Friday", "Saturday"]
+  dayname = DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}."
 end
