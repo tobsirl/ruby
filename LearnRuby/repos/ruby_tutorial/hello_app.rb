10a245a06e4ba3a845fba01edfcc21b63cf25a37
Paul Tobin <tobster111@gmail.com>
2022-07-07 18:03:33 +0100

    day_of_the_week

--- LearnRuby/repos/ruby_tutorial/hello_app.rb
+++ LearnRuby/repos/ruby_tutorial/hello_app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+# Returns the day of the week for the given Time object.
+def day_of_the_week(time)
+  Date::DAYNAMES[time.wday]
+end
+
 get '/' do
   DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
               "Thursday", "Friday", "Saturday"]
