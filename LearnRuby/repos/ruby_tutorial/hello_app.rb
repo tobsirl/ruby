ec832a15d35a4b79a8b9d0c7288e2126f573f517
Paul Tobin <tobster111@gmail.com>
2022-07-07 18:14:17 +0100

    Fixed

--- LearnRuby/repos/ruby_tutorial/hello_app.rb
+++ LearnRuby/repos/ruby_tutorial/hello_app.rb
@@ -1,10 +1,6 @@
 require 'sinatra'
-
-# Returns the day of the week for the given Time object.
-def day_of_the_week(time)
-  Date::DAYNAMES[time.wday]
-end
+require './day'
 
 get '/' do
-  "Hello, world! Happy #{day_of_the_week(Time.now)}."
+  "Hello, world! Happy #{day_of_the_week(Time.now)}—now from a file!"
 end
