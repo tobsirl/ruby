db80dc2da7a9dcf9d3ca7ef86cbbae7d23c45e7e
Paul Tobin <tobster111@gmail.com>
2022-07-07 18:04:21 +0100

    Using the function

--- LearnRuby/repos/ruby_tutorial/hello_app.rb
+++ LearnRuby/repos/ruby_tutorial/hello_app.rb
@@ -6,8 +6,5 @@ def day_of_the_week(time)
 end
 
 get '/' do
-  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
-              "Thursday", "Friday", "Saturday"]
-  dayname = DAYNAMES[Time.now.wday]
-  "Hello, world! Happy #{dayname}."
+  "Hello, world! Happy #{day_of_the_week(Time.now)}."
 end
