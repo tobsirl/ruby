a3323918406b24fcfe2da3b88b9ce7a7bff3dc43
Paul Tobin <tobster111@gmail.com>
2022-07-08 17:15:00 +0100

    funtional.rb symbol

--- LearnRuby/repos/ruby_tutorial/functional.rb
+++ LearnRuby/repos/ruby_tutorial/functional.rb
@@ -13,4 +13,8 @@ p imperative_urls(states)
 names = ["John", "Paul", "George", "Ringo"]
 
 ["ALICE", "BOB", "CHARLIE"].map { |name| name.downcase }
-names.map { |name| name.downcase }
+print_name = names.map { |name| name.downcase }
+p print_name
+
+print_name_symbol = names.map(&:downcase)
+p print_name_symbol
