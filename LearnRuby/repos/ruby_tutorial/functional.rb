7c8a31ad1c5cd6ff6d00b5ed2ff2b9d832d93a9f
Paul Tobin <tobster111@gmail.com>
2022-07-08 17:21:27 +0100

    funtional version urlify

--- LearnRuby/repos/ruby_tutorial/functional.rb
+++ LearnRuby/repos/ruby_tutorial/functional.rb
@@ -1,10 +1,16 @@
 states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
 
+# Returns a URL-friendly version of a string.
+#   Example: "North Dakota" -> "north-dakota"
+def urlify(string)
+  string.downcase.split.join('-')
+end
+
 # urls: Imperative version
 def imperative_urls(states)
   urls = []
   states.each do |state|
-    urls << state.downcase.split.join("-")
+    urls << urlify(state)
   end
   urls
 end
@@ -21,6 +27,7 @@ p print_name_symbol
 
 # urls: Functional version
 def functional_urls(states)
-  states.map { |state| state.downcase.split.join('-') }
+  states.map { |state| urlify(state) }
 end
 puts functional_urls(states).inspect
+
