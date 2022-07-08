0fb859dec3bc1f59b63762a77aa1c5dc629a3d4f
Paul Tobin <tobster111@gmail.com>
2022-07-08 15:50:06 +0100

    Github Copilot

--- LearnRuby/repos/ruby_tutorial/blocks.rb
+++ LearnRuby/repos/ruby_tutorial/blocks.rb
@@ -1,2 +1,6 @@
 # single line
 (1..5).each { |i| puts 2**i }
+
+(1..5).each do |i|
+  puts 2**i
+end
---
+++ LearnRuby/repos/ruby_tutorial/index.js
@@ -0,0 +1,7 @@
+function calculateDayBetweenDates(startDate, endDate) {
+  var start = new Date(startDate);
+  var end = new Date(endDate);
+  var diff = end.getTime() - start.getTime();
+  var days = diff / (1000 * 60 * 60 * 24);
+  return days;
+}
