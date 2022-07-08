b67eaf212bc12409c67fe8a450cd70cbd4a32e9e
Paul Tobin <tobster111@gmail.com>
2022-07-08 15:52:15 +0100

    Github Copilot 2

--- LearnRuby/repos/ruby_tutorial/index.js
+++ LearnRuby/repos/ruby_tutorial/index.js
@@ -5,3 +5,5 @@ function calculateDayBetweenDates(startDate, endDate) {
   var days = diff / (1000 * 60 * 60 * 24);
   return days;
 }
+
+['ant', 'bat', 'cat', 42].forEach((element) => console.log(element));
