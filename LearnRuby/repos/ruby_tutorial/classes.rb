39c6d0320f18408fcacf27560a5e19a9e186af91
Paul Tobin <tobster111@gmail.com>
2022-07-09 14:02:10 +0100

    removing self

--- LearnRuby/repos/ruby_tutorial/classes.rb
+++ LearnRuby/repos/ruby_tutorial/classes.rb
@@ -2,7 +2,7 @@
 class Phrase < String
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content = self.downcase
+    processed_content = downcase
     processed_content == processed_content.reverse
   end
 
