6036244b927860b1394932cdc44cbc0788801996
Paul Tobin <tobster111@gmail.com>
2022-07-09 11:41:28 +0100

    assign with .content

--- LearnRuby/repos/ruby_tutorial/classes.rb
+++ LearnRuby/repos/ruby_tutorial/classes.rb
@@ -1,7 +1,7 @@
 # Defines a Phrase class.
 class Phrase
   attr_accessor :content
-  
+
   def initialize(content)
     @content = content
   end
@@ -9,3 +9,6 @@ end
 
 phrase = Phrase.new("Madam, I'm Adam.")
 puts phrase.content 
+
+phrase.content = "Able was I, ere I saw Elba."
+puts phrase.content
