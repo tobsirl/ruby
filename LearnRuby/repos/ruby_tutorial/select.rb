0c05294aa119198c4528e05eaf89f3827c3185b3
Paul Tobin <tobster111@gmail.com>
2022-07-09 10:03:55 +0100

    Select states

--- LearnRuby/repos/ruby_tutorial/select.rb
+++ LearnRuby/repos/ruby_tutorial/select.rb
@@ -1,3 +1,23 @@
 [1, 2, 3, 4, 5, 6, 7, 8].select { |n| n % 2 == 0}
 
 [1, 2, 3, 4, 5, 6, 7, 8].select { |n| n * 2}
+
+states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
+
+# singles: Imperative version
+def imperative_singles(states)
+  singles = []
+  states.each do |state|
+    if (state.split.length == 1)
+      singles << state
+    end
+  end
+  singles
+end
+puts imperative_singles(states).inspect
+
+# singles: Functional version
+def functional_singles(states)
+  states.select { |state| state.split.length == 1 }
+end
+puts functional_singles(states).inspect
