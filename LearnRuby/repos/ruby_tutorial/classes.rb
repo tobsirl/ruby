aa3cfdff8081b527aec9b244375366a3187e9511
Paul Tobin <tobster111@gmail.com>
2022-07-09 11:27:00 +0100

    Content

--- LearnRuby/repos/ruby_tutorial/classes.rb
+++ LearnRuby/repos/ruby_tutorial/classes.rb
@@ -1,7 +1,9 @@
 # Defines a Phrase class.
 class Phrase
-  
+  def initialize(content)
+    @content = content
+  end
 end
 
-phrase = Phrase.new
-puts phrase
+phrase = Phrase.new("Madam, I'm Adam.")
+puts phrase.content 
