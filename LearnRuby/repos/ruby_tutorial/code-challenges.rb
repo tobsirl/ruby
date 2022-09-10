04d9caeea6f94023c0d011d46a37c8c8294d5b17
Paul Tobin <tobster111@gmail.com>
2022-09-10 20:27:31 +0100

    problem

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -10,3 +10,14 @@ def arithmetic(a, b, operator)
     a / b
   end
 end
+
+# Super Duper Easy
+# Make a function that returns the value multiplied by 50 and increased by 6. 
+# If the value entered is a string it should return "Error".
+def problem x
+  if x.is_a? String
+    "Error"
+  else
+    (x * 50) + 6
+  end
+end
