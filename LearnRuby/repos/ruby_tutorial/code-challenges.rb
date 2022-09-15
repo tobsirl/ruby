6a8c141a1614d703552da010dbc782ac5ce8a545
Paul Tobin <tobster111@gmail.com>
2022-09-15 18:09:12 +0100

    Task

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -48,3 +48,6 @@ end
 def grow(x)
   x.inject(:*)
 end
+
+# Task
+# Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
