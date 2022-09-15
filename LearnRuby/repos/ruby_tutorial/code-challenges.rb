dbfdaf90dafdfd73fd5aaef7911e5ae215d9dee4
Paul Tobin <tobster111@gmail.com>
2022-09-15 18:09:41 +0100

    solution

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -51,3 +51,6 @@ end
 
 # Task
 # Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+def min_value(digits)
+  digits.uniq.sort.join('').to_i
+end
