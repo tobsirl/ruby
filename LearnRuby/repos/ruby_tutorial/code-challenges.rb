29e90eb0dc74f50caf0d33f68647e89eef177f14
Paul Tobin <tobster111@gmail.com>
2022-09-17 12:24:58 +0100

    Code challenge solution

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -63,4 +63,6 @@ end
 # Sort array by string length 7kyu
 # Write a function that takes an array of strings as an argument and returns 
 # a sorted array containing the same strings, ordered from shortest to longest.
-
+def sort_by_length(arr)
+  arr.sort_by(&:length)
+end
