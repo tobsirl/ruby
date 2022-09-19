7ce3fb45a1c79c94d69124cfda74d0bce0e3d4b0
Paul Tobin <tobster111@gmail.com>
2022-09-19 19:03:06 +0100

    Solution tidy

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -86,3 +86,12 @@ def get_grade(s1, s2, s3)
    end
 end
 
+def get_grade(s1, s2, s3)
+  case (s1 + s2 + s3).fdiv 3
+  when 90..100 then 'A'
+  when 80...90 then 'B'
+  when 70...80 then 'C'
+  when 60...70 then 'D'
+  else 'F'
+  end
+end
