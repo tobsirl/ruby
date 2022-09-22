95a6e311ec6bc7a57078ad9fc0650e9c42a12c1b
Paul Tobin <tobster111@gmail.com>
2022-09-22 17:34:41 +0100

    Task

--- LearnRuby/repos/ruby_tutorial/code-challenges.rb
+++ LearnRuby/repos/ruby_tutorial/code-challenges.rb
@@ -130,3 +130,13 @@ end
 def arr(n = 0)
   n.times.to_a
 end
+
+# Total amount of points 8kyu
+# Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+# For example: ["3:1", "2:2", "0:1", ...]
+
+# Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+# if x > y: 3 points
+# if x < y: 0 point
+# if x = y: 1 point
